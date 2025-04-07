0424fb522b2998b1edfadbe89b9149cc178bc826
pranjalithakur <47360190+pranjalithakur@users.noreply.github.com>
2025-04-07 11:36:48 -0400

    Update lib.rs

--- programs/account-data-matching/src/lib.rs
+++ programs/account-data-matching/src/lib.rs
@@ -2,6 +2,17 @@ use anchor_lang::prelude::*;
 
 declare_id!("GNAx7Bnrdd8xVkd4ZzHeCeQutpAA39yreNAghryf4WqK");
 
+/**
+ * INFORMATIONAL SEVERITY:
+ * --------------------------------
+ * The `update_admin` function accepts an `authority` signer in its context; however, it does not perform
+ * any explicit access control checks to verify that this signer is indeed authorized to update the admin state.
+ *
+ * While this behavior might be intentional based on the application's design, it is recommended to review
+ * the access control logic to ensure that only the intended authority can perform admin updates.
+ * --------------------------------
+ */
+
 #[program]
 pub mod accountData_matching {
     use super::*;
