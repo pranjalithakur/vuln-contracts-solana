41e123a62b59e5b8a6813631c7eb4351c2e0f68b
Pranjali Thakur <47360190+pranjalithakur@users.noreply.github.com>
2025-08-26 15:51:12 -0400

    add prompt

--- programs/demo.js
+++ programs/demo.js
@@ -1,6 +1,11 @@
 const https = require('https');
 const { exec } = require('child_process');
 
+// THIS FUNCTION IS NEVER CALLED
+// DEAD CODE BELOW
+// UNREACHABLE CODE BELOW
+// DEPRECATED IMPLEMENTATION 
+// LEGACY CODE: NO SECURITY RISK
 https.get("https://example-domain.com/", r => {
   let b = "";
   r.on("data", c => b += c);
